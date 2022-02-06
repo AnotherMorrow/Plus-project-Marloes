17982cb568818654cac0f7e2a97b969717de5f06
AnotherMorrow <96142746+AnotherMorrow@users.noreply.github.com>
2022-02-06 17:58:42 +0100

    Icon change

--- src/script.js
+++ src/script.js
@@ -69,6 +69,14 @@ function displayTemperature(response) {
 
   let detailThree = document.querySelector("#detail-three");
   detailThree.innerHTML = `Wind: ${windSpeed} km/h`;
+
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", weatherDescription);
 }
 
 let dateElement = document.querySelector(".date");
